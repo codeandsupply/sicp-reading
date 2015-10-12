16aafd0dda1a3d70ea9cdaa8f64d9160a7d60495
Erik Swanson <theerikswanson@gmail.com>
2015-10-11 23:35:43 -0400

    Use sicp language pack

--- Exercises/Erik/Section1-2/Exercise1-9.rkt
+++ Exercises/Erik/Section1-2/Exercise1-9.rkt
@@ -1,3 +1,5 @@
+#lang planet neil/sicp
+
 ;; Exercise 1.9. Each of the following two procedures defines a method
 ;; for adding two positive integers in terms of the procedures inc,
 ;; which increments its argument by 1, and dec, which decrements its
