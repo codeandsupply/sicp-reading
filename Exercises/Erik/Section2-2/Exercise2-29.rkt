0579a9d624d31208f0d7515f5dd58cf8d47dc6a4
Erik Swanson <theerikswanson@gmail.com>
2015-11-07 12:31:46 -0500

    Exercise 2.29

---
+++ Exercises/Erik/Section2-2/Exercise2-29.rkt
@@ -0,0 +1,103 @@
+#lang planet neil/sicp
+
+;; Exercise 2.29. A binary mobile consists of two branches, a left
+;; branch and a right branch. Each branch is a rod of a certain
+;; length, from which hangs either a weight or another binary
+;; mobile. We can represent a binary mobile using compound data by
+;; constructing it from two branches (for example, using list):
+
+(define (make-mobile left right)
+  (list left right))
+
+;; A branch is constructed from a length (which must be a number)
+;; together with a structure, which may be either a number
+;; (representing a simple weight) or another mobile:
+
+(define (make-branch length structure)
+  (list length structure))
+
+;; a. Write the corresponding selectors left-branch and right-branch,
+;; which return the branches of a mobile, and branch-length and
+;; branch-structure, which return the components of a branch.
+
+(define (left-branch mobile)
+  (car mobile))
+(define (right-branch mobile)
+  (car (cdr mobile)))
+(define (branch-length branch)
+  (car branch))
+(define (branch-structure branch)
+  (car (cdr branch)))
+
+(branch-length (make-branch 1 2))
+(branch-structure (make-branch 1 2))
+
+;; b. Using your selectors, define a procedure total-weight that
+;; returns the total weight of a mobile.
+
+(define (total-weight mobile)
+  (define (branch-weight branch)
+    (if (pair? (branch-structure branch))
+        (total-weight (branch-structure branch))
+        (branch-structure branch)))
+  (if (pair? mobile)
+      (+ (branch-weight (left-branch mobile))
+         (branch-weight (right-branch mobile)))
+      mobile))
+
+(define ll (make-branch 3 1))
+(define lr (make-branch 2 6))
+(define rl (make-branch 1 4))
+(define rr (make-branch 1 2))
+(define l (make-branch 1 (make-mobile ll lr)))
+(define r (make-branch 3 (make-mobile rl rr)))
+(define my-mobile (make-mobile l r))
+
+(total-weight my-mobile)
+(total-weight 12)
+
+;; c. A mobile is said to be balanced if the torque applied by its
+;; top-left branch is equal to that applied by its top-right branch
+;; (that is, if the length of the left rod multiplied by the weight
+;; hanging from that rod is equal to the corresponding product for the
+;; right side) and if each of the submobiles hanging off its branches
+;; is balanced. Design a predicate that tests whether a binary mobile
+;; is balanced.
+
+(define (torque branch)
+  (* (branch-length branch)
+     (total-weight (branch-structure branch))))
+
+(define (balanced? mobile)
+  (cond ((not (pair? mobile)) #t)
+        ((not (= (torque (left-branch mobile))
+                 (torque (right-branch mobile)))) #f)
+        (else (and (balanced? (branch-structure (left-branch mobile)))
+                   (balanced? (branch-structure (right-branch mobile)))))))
+
+(balanced? my-mobile)
+
+(define my-balanced-mobile
+  (make-mobile (make-branch 3 (make-mobile (make-branch 4 2)
+                                           (make-branch 2 4)))
+               (make-branch 6 (make-mobile (make-branch 1 2)
+                                           (make-branch 2 1)))))
+
+(balanced? my-balanced-mobile)
+
+;; d. Suppose we change the representation of mobiles so that the
+;; constructors are
+
+;;     (define (make-mobile left right)
+;;       (cons left right))
+;;     (define (make-branch length structure)
+;;       (cons length structure))
+
+;; How much do you need to change your programs to convert to the new
+;; representation?
+
+;; You simply need to change your definitions for the selectors
+;; left-branch, right-branch, branch-length, and branch-structure
+;;
+;; Specifically, you would need to remove the extra `car` step from
+;; right-branch and branch-length
