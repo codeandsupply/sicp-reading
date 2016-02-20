7acc4d7fcc0f400320203344b7a37c67ed6e6737
Erik Swanson <theerikswanson@gmail.com>
2016-02-19 21:20:56 -0500

    More Section 3.3 notes

--- Notes/Erik/Section-3-3-Notes.rkt
+++ Notes/Erik/Section-3-3-Notes.rkt
@@ -121,3 +121,119 @@
                               (cons key-2 value))
                         (mcdr table)))))
   'ok)
+
+;; 3.3.4 A simulation for digital circuits
+
+;; Our primitives for building this simulation are:
+;;
+;; - Wires
+;; - inverters
+;; - and-gates
+;; - or-gates
+
+;; We're going to need the following primitives on wires:
+;;
+;;     (make-wire)
+;;     (get-signal <wire>)
+;;     (set-signal! <wire>)
+;;     (add-action! <wire> <new value>)
+
+;; With these we can define inverters, and the and-gate
+
+;; To model this, we're going to register a function on an input wire,
+;; to be run whenever the signal on that wire changes.
+
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= (signal-value new-value)))
+          (begin
+            (set! signal-value new-value)
+            (call-each action-procedures)
+            'done)))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc))
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknown operation -- WIRE" m))))
+    dispatch))
+
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (or-gate o1 o2 output)
+  (define (or-action-procedure)
+    (let ((new-value (logical-or (get-signal o2) (get-signal o2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! o1 or-action-procedure)
+  (add-action! o2 or-action-procedure)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+(define (logical-and s1 s2)
+  (cond ((and (= s1 1) (= s2 1)) 1)
+        ((or (= s1 0) (= s2 0)) 0)
+        (else (error "Invalid signal" s))))
+(define (logical-or s1 s2)
+  (cond ((or (= s1 1) (= s2 1)) 1)
+        ((and (= s1 0) (= s2 0)) 0)
+        (else (error "Invalid signal" s))))
+
+
+(define a (make-wire))
+(define b (make-wire))
+(define c (make-wire))
+
+(define d (make-wire))
+(define e (make-wire))
+(define s (make-wire))
+
+;; We could make a half adder like this:
+;;
+;;     (or-gate a b d)
+;;     (and-gate a b c)
+;;     (inverter c e)
+;;     (and-gate d e s)
+
+;; Or we could name the function that does it, like this:
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c1 c-out)
+    'ok))
