a74a2897290727344e077a5ad1499fc08ed351a0
Erik Swanson <theerikswanson@gmail.com>
2016-02-28 13:16:02 -0500

    Notes

--- Notes/Erik/Section-3-5-Notes.rkt
+++ Notes/Erik/Section-3-5-Notes.rkt
@@ -148,8 +148,8 @@
 
 
 (define fibs
-  (cons-stream 0
-               (cons-stream 1
+  (stream-cons 0
+               (stream-cons 1
                             (add-streams (stream-cdr fibs)
                                          fibs))))
 
