48daabcf5a9198eb9fca61ead4960f3db027f316
Erik Swanson <theerikswanson@gmail.com>
2016-03-13 11:26:21 -0400

    Exercise 4.6

---
+++ Exercises/Erik/Section4-1/Exercise4-06.rkt
@@ -0,0 +1,33 @@
+;; Exercise 4.6.  Let expressions are derived expressions, because
+
+;; (let ((<var1> <exp1>) ... (<varn> <expn>))
+;;   <body>)
+
+;; is equivalent to
+
+;; ((lambda (<var1> ... <varn>)
+;;    <body>)
+;;  <exp1>
+
+;;  <expn>)
+
+;; Implement a syntactic transformation let->combination that reduces
+;; evaluating let expressions to evaluating combinations of the type
+;; shown above, and add the appropriate clause to eval to handle let
+;; expressions.
+
+(define (let->combination exp env)
+  (meta-apply (make-procedure (let-parameters exp)
+                              (let-body exp)
+                              env)
+              (let-values exp)))
+
+(define (let? exp)
+  (tagged-list? exp 'let))
+
+(define (let-body exp) (caddr exp))
+(define (let-arguments) (cadr exp))
+(define (let-parameters exp)
+  (map car (let-arguments exp)))
+(define (let-values exp)
+  (map cadr (let-arguments exp)))
