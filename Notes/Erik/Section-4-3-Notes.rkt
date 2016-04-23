49a5d997e2b5a0b2b18d85aa6de87d5a25b6d6b8
Erik Swanson <theerikswanson@gmail.com>
2016-04-23 19:23:47 -0400

    More notes

--- Notes/Erik/Section-4-3-Notes.rkt
+++ Notes/Erik/Section-4-3-Notes.rkt
@@ -45,6 +45,14 @@
 
 ;; Here we can solve the multiple dwelling problem:
 
+;; Baker, Cooper, Fletcher, Miller, and Smith live on different floors
+;; of an apartment house that contains only five floors. Baker does
+;; not live on the top floor. Cooper does not live on the bottom
+;; floor. Fletcher does not live on either the top or the bottom
+;; floor. Miller lives on a higher floor than does Cooper. Smith does
+;; not live on a floor adjacent to Fletcher's. Fletcher does not live
+;; on a floor adjacent to Cooper's. Where does everyone live?
+
 (define (multiple-dwelling)
   (let ((baker (amb 1 2 3 4 5))
         (cooper (amb 1 2 3 4 5))
@@ -66,4 +74,41 @@
           (list 'miller miller)
           (list 'smith smith))))
 
-;; This should produce
+;; This should produce:
+
+'((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1))
+
+
+;; We can also do simple natural language parsing.
+
+;; Start by defining examples of parts of speech:
+
+(define nouns '(noun student professor cat class))
+(define verbs '(verb studies lectures eats sleeps))
+(define articles '(article the a))
+
+;; And then we define a grammar:
+
+(define (parse-sentence)
+  (list 'sentence
+         (parse-noun-phrase)
+         (parse-word verbs)))
+
+(define (parse-noun-phrase)
+  (list 'noun-phrase
+        (parse-word articles)
+        (parse-word nouns)))
+
+(define (parse-word word-list)
+  (require (not (null? *unparsed*)))
+  (require (memq (car *unparsed*) (cdr word-list)))
+  (let ((found-word (car *unparsed*)))
+    (set! *unparsed* (cdr *unparsed*))
+    (list (car word-list) found-word)))
+
+(define *unparsed* '())
+(define (parse input)
+  (set! *unparsed* input)
+  (let ((sent (parse-sentence)))
+    (require (null? *unparsed*))
+    sent))
